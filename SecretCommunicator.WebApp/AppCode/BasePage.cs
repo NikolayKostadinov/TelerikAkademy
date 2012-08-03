d6da2e15246cd40e909d0e7985b192b9e055b289
Saykor <saykor@gmail.com>
2012-08-03 13:01:49 +0300

    Initial commit

---
+++ SecretCommunicator.WebApp/AppCode/BasePage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SecretCommunicator.WebData;
+
+namespace SecretCommunicator.WebApp.AppCode
+{
+    public class BasePage : System.Web.UI.Page
+    {
+        boSessionState _sessionState = new boSessionState();
+        public boSessionState SessionState
+        {
+            get { return _sessionState; }
+            set { _sessionState = value; }
+        }
+
+        #region Page Events
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            if (Session["sessionState"] != null)
+                _sessionState = (boSessionState)Session["sessionState"];
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPreRenderComplete(EventArgs e)
+        {
+            base.OnPreRenderComplete(e);
+            SaveSession();
+        }
+        #endregion // Page Events
+
+        protected void SaveSession()
+        {
+            Session["sessionState"] = _sessionState;
+        }
+    }
+}
