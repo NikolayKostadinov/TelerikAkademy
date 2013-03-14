799b048dbe32fb66b99587a4e49d37f071f7d947
saykor <saykor@gmail.com>
2013-03-14 15:54:10 +0200

    Add JavaScriptDevelopment project

---
+++ JavaScriptDevelopment/JavaScriptDevelopment/ConditionalStatements/script.js
@@ -0,0 +1,239 @@
+﻿$(document).ready(function () {
+
+    $('#brnExchangeValue').click(function () {
+        var first = parseInt($('#txtExchangeValue1').val());
+        var second = parseInt($('#txtExchangeValue2').val());
+        if (first > second)
+        {
+            first += second;
+            second = first - second;
+            first -= second;
+        }
+        $('#pnlExchangeValue').text(first + " " + second);
+    });
+
+    $('#btnShowNumberSign').click(function () {
+        var first = parseInt($('#txtShowNumberSign1').val());
+        var second = parseInt($('#txtShowNumberSign2').val());
+        var third = parseInt($('#txtShowNumberSign3').val());
+        var result = "+";
+        if (first < 0 || second < 0 || third < 0)
+            result = "-";
+        $('#pnlShowNumberSign').text(result);
+    });
+
+    $('#btnFindBiggerNumber').click(function () {
+        var first = parseInt($('#txtFindBiggerNumber1').val());
+        var second = parseInt($('#txtFindBiggerNumber2').val());
+        var third = parseInt($('#txtFindBiggerNumber3').val());
+        var result = third;
+        if (first > second) {
+            if (first > third)
+                result = first;
+        }
+        else {
+            if (second > third)
+                result = second;
+        }
+        $('#pnlFindBiggerNumber').text(result);
+    });
+
+    $('#btnOrderByDescending').click(function () {
+        var a = parseInt($('#txtOrderByDescending1').val());
+        var b = parseInt($('#txtOrderByDescending2').val());
+        var c = parseInt($('#txtOrderByDescending3').val());
+        var result;
+        if (a > b) {
+            if (a > c) {
+                result = a;
+                if (b > c) {
+                    result = result + " " + b;
+                    result = result + " " + c;
+                }
+                else {
+                    result = result + " " + c;
+                    result = result + " " + b;
+                }
+            }
+            else {
+                result = c;
+                result = result + " " + a;
+                result = result + " " + b;
+            }
+        }
+        else {
+            if (b > c) {
+                result = b;
+                if (a > c) {
+                    result = result + " " + a;
+                    result = result + " " + c;
+                }
+                else {
+                    result = result + " " + c;
+                    result = result + " " + a;
+                }
+            }
+            else {
+                result = c;
+                result = result + " " + b;
+                result = result + " " + a;
+            }
+        }
+        $('#pnlOrderByDescending').text(result);
+    });
+
+    $('#btnNameOfDigit').click(function () {
+        var source = parseInt($('#txtNameOfDigit').val());
+        var result;
+        switch (source)
+        {
+            case 0:
+                result = "Zero";
+                break;
+            case 1:
+                result = "One";
+                break;
+            case 2:
+                result = "Two";
+                break;
+            case 3:
+                result = "Three";
+                break;
+            case 4:
+                result = "Four";
+                break;
+            case 5:
+                result = "Five";
+                break;
+            case 6:
+                result = "Six";
+                break;
+            case 7:
+                result = "Seven";
+                break;
+            case 8:
+                result = "Eight";
+                break;
+            case 9:
+                result = "Nine";
+                break;
+            default:
+                result = "Unknown digit!";
+                break;
+        }
+        $('#pnlNameOfDigit').text(result);
+    });
+
+    $('#btnSolveQuadratic').click(function () {
+        var a = parseInt($('#txtSolveQuadraticA').val());
+        var b = parseInt($('#txtSolveQuadraticB').val());
+        var c = parseInt($('#txtSolveQuadraticC').val());
+        var result;
+        //Quadratic Formula: x = (-b +- sqrt(b^2 - 4ac)) / 2a
+        var insideSquareRoot = (b * b) - 4 * a * c;
+        if (insideSquareRoot < 0)
+        {
+            result = "No Solution";
+        }
+        else
+        {
+            var sqrt = Math.sqrt(insideSquareRoot);
+            var x1 = (-b + sqrt) / (2 * a);
+            var x2 = (-b - sqrt) / (2 * a);
+            if (x1 == x2)
+                result = "Only one solution: " + x1;
+            else
+                result = "2 Solutions: " + x1 + " and " + x2;
+        }
+        $('#pnlSolveQuadratic').text(result);
+    });
+
+    $('#btnFindGreatestNumber').click(function () {
+        var source = $('#txtFindGreatestNumber').val();
+        var n = source.split(",");
+        var result = 0;
+
+        for (var i = 0; i < n.length; i++) {
+            if (result == 0)
+                result = n[i];
+            else
+            {
+                if (result < n[i])
+                    result = n[i];
+            }
+        }
+
+        $('#pnlFindGreatestNumber').text(result);
+    });
+
+    $('#btnConvertNumberstToText').click(function () {
+        var source = parseInt($('#txtConvertNumberstToText').val());
+        var digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirdtheen", "fourthen", "fiftheen", "sixtheen", "seventheen", "eightheen", "ninetheen"];
+        var dec = ["and", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+        var sb = [];
+
+        var iArray = source.toString().split('');
+        switch (iArray.length) {
+            case 1:
+                sb.push(digits[parseInt(iArray[0])] + " ");
+                break;
+                case 2:
+                    if (source < 20)
+                        sb.push(digits[source]);
+                    else
+                    {
+                        switch (parseInt(iArray[source]))
+                        {
+                            case 0:
+                                sb.push(dec[parseInt(iArray[0])] + " ");
+                                break;
+                            default:
+                                sb.push(dec[parseInt(iArray[0])] + "-" + digits[parseInt(iArray[1])] + " ");
+                                break;
+                        }
+                    }
+                    break;
+                case 3:
+                    if (parseInt(iArray[1]) == 0 && parseInt(iArray[2]) == 0)
+                        sb.push(digits[parseInt(iArray[0])] + " hundred" + " ");
+                    else
+                    {
+                        for (var j = 0; j < iArray.length; j++)
+                        {
+                            switch (j)
+                            {
+                                case 0:
+                                    sb.push(digits[parseInt(iArray[j])] + " hundred" + " ");
+                                    break;
+                                default:
+                                    if (j == 1)
+                                    {
+                                        if (parseInt(iArray[j]) == 0)
+                                        {
+                                            sb.push(" and " + digits[parseInt(iArray[j + 1])] + " ");
+                                        }
+                                        else if (parseInt(iArray[j]) == 1)
+                                        {
+                                            sb.push(" and " + digits[10 + parseInt(iArray[j + 1])] + " ");
+                                        }
+                                        else if (parseInt(iArray[j + 1]) == 0)
+                                        {
+                                            sb.push(" " + dec[parseInt(iArray[j])] + " ");
+                                        }
+                                        else
+                                        {
+                                            sb.push(" " + dec[parseInt(iArray[j])] + "-" + digits[parseInt(iArray[j + 1])] + " ");
+                                        }
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+            default:
+                break;
+        }
+        $('#pnlConvertNumberstToText').text(sb.toString());
+    });
+
+});
---
+++ JavaScriptDevelopment/JavaScriptDevelopment/OperatorsExpressions/script.js
@@ -0,0 +1,97 @@
+﻿$(document).ready(function () {
+
+    $('#btnIsOdd').click(function () {
+        var i = parseInt($('#txtIsOdd').val());
+        var result = false;
+        if (i % 2 == 0)
+            result = true;
+        $('#pnlIsOdd').text(result);
+    });
+
+    $('#btnDividedWithoutRemainder').click(function () {
+        var i = parseInt($('#txtDividedWithoutRemainder').val());
+        var result = false;
+        if (i % 7 == 0 && i % 5 == 0)
+            result = true;
+        $('#pnlDividedWithoutRemainder').text(result);
+    });
+
+    $('#btnCalculateRectangle').click(function () {
+        var w = parseInt($('#txtWidth').val());
+        var h = parseInt($('#txtHeight').val());
+        $('#pnlCalculateRectangle').text(w * h);
+    });
+
+    $('#btnIsThirdDigitIsSeven').click(function () {
+        var i = parseInt($('#txtIsThirdDigitIsSeven').val()).toString();
+        var result = false;
+        if (i.length > 3)
+        {
+            if(i.split('').reverse()[2] == 7)
+                result = true;
+        }
+            
+        $('#pnlIsThirdDigitIsSeven').text(result);
+    });
+
+    $('#btnGetBitByPosition').click(function () {
+        var i = parseInt($('#txtGetBitByPosition').val());
+        var result = false;
+
+        var mask = 1 << 3;
+        var nAndMask = i & mask;
+        result = nAndMask >> 3;
+
+        $('#pnlGetBitByPosition').text(result);
+    });
+
+    $('#btnIsInCircle').click(function () {
+        var x = parseInt($('#txtIsInCircleX').val());
+        var y = parseInt($('#txtIsInCircleY').val());
+        var result = false;
+        if (Math.sqrt(x - 0) + Math.sqrt(y - 0) <= Math.sqrt(5))
+            result = true;
+        $('#pnlIsInCircle').text(result);
+    });
+
+    $('#btnIsPrime').click(function () {
+        var source = parseInt($('#txtIsPrime').val());
+        var result = true;
+        if (source <= 1)
+            result = false;
+        else {
+            for (var i = 2; i < Math.sqrt(source) ; i++) {
+                if (source % i == 0) {
+                    result = false;
+                }
+            }
+        }
+        $('#pnlIsPrime').text(result);
+    });
+
+    $('#btnCalculatesTrapezoid').click(function () {
+        var a = parseInt($('#txtCalculatesTrapezoidA').val());
+        var b = parseInt($('#txtCalculatesTrapezoidB').val());
+        var h = parseInt($('#txtCalculatesTrapezoidH').val());
+        var result = (a + b) / 2 * h;
+        $('#pnlCalculatesTrapezoid').text(result);
+    });
+
+    $('#btnIsPointInTheCircleOutRectangle').click(function () {
+        var x = parseInt($('#txtIsPointInTheCircleOutRectangleX').val());
+        var y = parseInt($('#txtIsPointInTheCircleOutRectangleY').val());
+        var result = false;
+
+        var inCircle = false;
+        if (Math.sqrt(x - 1) + Math.sqrt(y - 1) <= 3)
+            inCircle = true;
+
+        var inRectangle = false;
+        if ((1 <= x && x <= 6) && (-1 <= y  && y <= 2))
+            inRectangle = true;
+
+        if(inCircle && inRectangle)
+            result = true;
+        $('#pnlIsPointInTheCircleOutRectangle').text(result);
+    });
+});
