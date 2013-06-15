6a8791842e76567a66e867c2ac20a20872eb797f
saykor <saykor@gmail.com>
2013-06-15 23:18:00 +0300

    add jquery overview

---
+++ CSharpDevelopment/DataStructureAndAlgorithms/AdvancedDataStructure/ImplementTrie/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ImplementTrie
+{
+    class Program
+    {
+        private static void Main()
+        {
+            try
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                string inputText;
+                StreamReader sr = new StreamReader(@"../../text.txt");
+                using (sr)
+                {
+                    inputText = sr.ReadToEnd().ToLower();
+                }
+                var matches = Regex.Matches(inputText, @"[a-zA-Z]+");
+
+                stopwatch.Stop();
+                Console.WriteLine("Read from txt file and get words with regex: {0}", stopwatch.Elapsed);
+
+                stopwatch.Reset();
+                stopwatch.Restart();
+
+                Tries t = new Tries();
+                t.Insert(string.Join(" ", matches.Cast<Match>().Select(s => s.Value)));
+
+                stopwatch.Stop();
+                Console.WriteLine("insert words in trie: {0}", stopwatch.Elapsed);
+
+                stopwatch.Reset();
+                stopwatch.Restart();
+
+                t.Contains("he");
+
+                stopwatch.Stop();
+                Console.WriteLine("search for word 'he': {0}", stopwatch.Elapsed);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CSharpDevelopment/DataStructureAndAlgorithms/AdvancedDataStructure/ImplementTrie/TrieNode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImplementTrie
+{
+    class TrieNode
+    {
+        public TrieNode[] nodes;
+        public bool isEnd = false;
+        public const int ASCIIA = 97;
+        public TrieNode()
+        {
+            nodes = new TrieNode[26];
+        }
+        public bool Contains(char c)
+        {
+            int n = Convert.ToByte(c) - ASCIIA;
+            if (n < 26)
+                return (nodes[n] != null);
+            else
+                return false;
+        }
+        public TrieNode GetChild(char c)
+        {
+            int n = Convert.ToByte(c) - ASCIIA;
+            return nodes[n];
+        }
+    }
+}
