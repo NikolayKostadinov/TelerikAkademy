459ed41c064ebcb48219f21ac1a5c847e78b278b
saykor <saykor@gmail.com>
2013-06-18 18:28:18 +0300

    find large area

---
+++ CSharpDevelopment/DataStructureAndAlgorithms/Recursion/FindLargeArea/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindLargeArea
+{
+    class Program
+    {
+        static string[,] labyrinth = 
+        {
+            {" ", " ", " ", "*", " ", " ", " "},
+            {" ", " ", " ", "*", " ", " ", " "},
+            {" ", " ", " ", " ", "*", " ", " "},
+            {" ", " ", " ", " ", "*", " ", " "},
+            {" ", " ", " ", " ", "*", " ", " "},
+        };
+
+        private static int maxSum = 0;
+
+        static void Main()
+        {
+            
+            for (int row = 0; row < labyrinth.GetLength(0); row++)
+            {
+                for (int col = 0; col < labyrinth.GetLength(1); col++)
+                {
+                    if (labyrinth[row, col] == " ")
+                    {
+                        DFS(row, col, 0);
+                    }
+                }
+            }
+            Console.WriteLine(maxSum);
+        }
+
+        static int DFS(int row, int col, int count)
+        {
+            if (count > maxSum)
+            {
+                maxSum = count;
+            }
+            if (row < 0 || col < 0 ||
+                row >= labyrinth.GetLength(0) || col >= labyrinth.GetLength(1) ||
+                labyrinth[row, col] != " ")
+            {
+                return 0;
+            }
+
+            labyrinth[row, col] = count.ToString();
+
+            count++;
+            DFS(row, col - 1, count);
+            DFS(row - 1, col, count);
+            DFS(row, col + 1, count);
+            DFS(row + 1, col, count);
+
+            return count;
+        }
+    }
+}
