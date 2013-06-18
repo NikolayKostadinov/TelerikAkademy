ed9a9df51e4cc6f8923081a4531773f45b82531b
saykor <saykor@gmail.com>
2013-06-18 22:07:50 +0300

    Add permutation and EughtQueensPuzzle

---
+++ CSharpDevelopment/DataStructureAndAlgorithms/Recursion/EightQueensPuzzle/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace EightQueensPuzzle
+{
+    class Program
+    {
+        private static int queens = 8;
+        private static int matrixLenght = 8;
+        static int[,] matrix = new int[matrixLenght, matrixLenght];
+        static void Main()
+        {
+            int startPosition = 0;
+            while(queens > 0)
+            {
+                PutQueen(startPosition, 0);
+                Start(0);
+                startPosition++;
+            }
+        }
+
+        private static void Start(int startPosition)
+        {
+            for (int row = startPosition; row < matrixLenght; row++)
+            {
+                for (int col = 0; col < matrixLenght; col++)
+                {
+                    if (matrix[row, col] == 0)
+                    {
+                        PutQueen(row, col);
+                        Print();
+                        Console.WriteLine();
+                    }
+                }
+            }
+            if (queens > 0)
+            {
+                queens = 8;
+                matrix = new int[matrixLenght, matrixLenght];
+            }
+        }
+
+        private static void Print()
+        {
+            for (int row = 0; row < matrixLenght; row++)
+            {
+                for (int col = 0; col < matrixLenght; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void PutQueen(int row, int col)
+        {
+            queens--;
+            matrix[row, col] = 9;
+            SetBeatenField(row, col);
+        }
+  
+        private static void SetBeatenField(int row, int col)
+        {
+            //cols
+            for (int i = 0; i < matrixLenght; i++)
+            {
+                if(matrix[row, i] < 9)
+                {
+                    matrix[row, i]++;
+                }
+            }
+
+            //rows
+            for (int i = 0; i < matrixLenght; i++)
+            {
+                if (matrix[i, col] < 9)
+                {
+                    matrix[i, col]++;
+                }
+            }
+
+            //topleft
+            var startRow = 0;
+            var startCol = 0;
+            startCol = col - row;
+            for (int i = 0; i < matrixLenght; i++)
+            {
+                if (startRow + i < matrixLenght && startCol + i < matrixLenght && startCol + i >=0 && matrix[startRow + i, startCol + i] < 9)
+                {
+                    matrix[startRow + i, startCol + i]++;
+                }
+            }
+
+            //topright
+            startCol = col + row;
+            if (startCol >= matrixLenght)
+            {
+                startCol = matrixLenght - 1;
+            }
+            for (int i = 0; i < matrixLenght; i++)
+            {
+                if (startRow + i < matrixLenght && startCol - i >= 0 && matrix[startRow + i, startCol - i] < 9)
+                {
+                    matrix[startRow + i, startCol - i]++;
+                }
+            }
+        }
+    }
+}
---
+++ CSharpDevelopment/DataStructureAndAlgorithms/Recursion/Permutation/Program.cs
@@ -0,0 +1,66 @@
+﻿//http://hardprogrammer.blogspot.com/2006/11/permutaciones-con-repeticin.html
+using System;
+
+namespace Permutation
+{
+    class Program
+    {
+        private static void Main()
+        {
+            int n = 3;
+            int[] numbers = new int[3];
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            Permutations(numbers);
+        }
+
+        private static void Permutations(int[] numbersSet)
+        {
+            Array.Sort(numbersSet);
+            Permute(numbersSet, 0, numbersSet.Length);
+        }
+
+        private static void Permute(int[] numbersSet, int start, int end)
+        {
+            PrintNumbers(numbersSet);
+
+            int swapValue = 0;
+
+            if (start < end)
+            {
+                for (int i = end - 2; i >= start; i--)
+                {
+                    for (int j = i + 1; j < end; j++)
+                    {
+                        if (numbersSet[i] != numbersSet[j])
+                        {
+                            swapValue = numbersSet[i];
+                            numbersSet[i] = numbersSet[j];
+                            numbersSet[j] = swapValue;
+
+                            Permute(numbersSet, i + 1, end);
+                        }
+                    }
+
+                    swapValue = numbersSet[i];
+                    for (int k = i; k < end - 1; )
+                    {
+                        numbersSet[k] = numbersSet[++k];
+                    }
+                    numbersSet[end - 1] = swapValue;
+                }
+            }
+        }
+
+        private static void PrintNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
