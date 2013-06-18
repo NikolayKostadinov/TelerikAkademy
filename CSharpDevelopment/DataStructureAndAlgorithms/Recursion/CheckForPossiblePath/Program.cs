d2ecc021ecb40eb3c2cf302d28af43ced90588c8
saykor <saykor@gmail.com>
2013-06-18 17:17:44 +0300

    Add CheckForPossiblePath

---
+++ CSharpDevelopment/DataStructureAndAlgorithms/Recursion/CheckForPossiblePath/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace CheckForPossiblePath
+{
+    class Program
+    {
+        static string[,] labyrinth = 
+        {
+            {" ", " ", " ", "*", " ", " ", " "},
+            {"*", "*", " ", "*", " ", "*", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+            {" ", "*", "*", "*", "*", "*", " "},
+            {" ", " ", " ", " ", " ", " ", " "},
+        };
+
+        static void Main()
+        {
+            labyrinth[4, 6] = "e";
+            Stack<int> stack = new Stack<int>();
+            CheckPath(0, 0, 0, stack);
+        }
+
+        private static bool CheckPath(int row, int col, int count, Stack<int> stack)
+        {
+            if (row < 0 || col < 0 ||
+                row >= labyrinth.GetLength(0) || col >= labyrinth.GetLength(1))
+            {
+                return false;
+            }
+
+            if (labyrinth[row, col] == "e")
+            {
+                return true;
+            }
+
+            if (labyrinth[row, col] != " ")
+            {
+                return false;
+            }
+
+            stack.Push(count);
+            labyrinth[row, col] = count.ToString();
+            count++;
+            if (CheckPath(row, col - 1, count, stack))
+            {
+                return true;
+            }
+            
+            if (CheckPath(row - 1, col, count, stack))
+            {
+                return true;
+            }
+
+            if (CheckPath(row, col + 1, count, stack))
+            {
+                return true;
+            }
+
+            if (CheckPath(row + 1, col, count, stack))
+            {
+                return true;
+            }
+
+            labyrinth[row, col] = " ";
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+            }
+            return false;
+        }
+    }
+}
