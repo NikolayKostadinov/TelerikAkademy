978c9d23a065a37e7023be1ffb9c6886d8d14661
saykor <saykor@gmail.com>
2013-06-23 23:15:29 +0300

    add FriendsInPesho

---
+++ CSharpDevelopmentExams/DataStructureAndAlgorithms/Graphs/FriendsInPesho/Connection.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FriendsInPesho
+{
+    internal struct Connection
+    {
+        public Node ToNode { get; set; }
+        public int Distance { get; set; }
+
+        public Connection(Node toNode, int distance)
+            : this()
+        {
+            this.ToNode = toNode;
+            this.Distance = distance;
+        }
+    }
+}
