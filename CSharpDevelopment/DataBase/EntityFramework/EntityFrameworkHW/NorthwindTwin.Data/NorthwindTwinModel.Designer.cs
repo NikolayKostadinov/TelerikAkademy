2119f3b760f495e43e23939485a58a8c43437d76
saykor <saykor@gmail.com>
2013-07-13 20:37:09 +0300

    Revert "Add Darabase Ado.Net"
    This reverts commit c0a1e66db1b0be11fd4a22d04f30c624391d5f73.

--- CSharpDevelopment/DataBase/EntityFramework/EntityFrameworkHW/NorthwindTwin.Data/NorthwindTwinModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\Projects\VisualStudio2012\Telerik\TelerikAkademy\CSharpDevelopment\DataBase\EntityFramework\EntityFrameworkHW\NorthwindTwin.Data\NorthwindTwinModel.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
