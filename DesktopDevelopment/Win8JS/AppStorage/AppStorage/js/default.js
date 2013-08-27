23ad53915cef203298b5d73a952514243b79d0d2
Dimitar Todorov <saykor@gmail.com>
2013-08-27 19:11:41 +0300

    update appStorage

--- DesktopDevelopment/Win8JS/AppStorage/AppStorage/js/default.js
+++ DesktopDevelopment/Win8JS/AppStorage/AppStorage/js/default.js
@@ -1,5 +1,5 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkId=232509
+﻿// For an introduction to the Navigation template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232506
 (function () {
     "use strict";
 
@@ -7,10 +7,9 @@
 
     var app = WinJS.Application;
     var activation = Windows.ApplicationModel.Activation;
+    var nav = WinJS.Navigation;
 
-    var localFolder = Windows.Storage.ApplicationData.current.localFolder;
-    
-    app.onactivated = function (args) {
+    app.addEventListener("activated", function (args) {
         if (args.detail.kind === activation.ActivationKind.launch) {
             if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
                 // TODO: This application has been newly launched. Initialize
@@ -19,81 +18,33 @@
                 // TODO: This application has been reactivated from suspension.
                 // Restore application state here.
             }
-            args.setPromise(WinJS.UI.processAll());
 
-            var appCache = new AppCache("workers.txt");
+            if (app.sessionState.history) {
+                nav.history = app.sessionState.history;
+            }
+            args.setPromise(WinJS.UI.processAll().then(function () {
 
-            
-            var calculate = new PrimeNumbersCalculator();
-
-            var btnCalculatePrimeNumberTo = document.getElementById("btnCalculatePrimeNumberTo");
-            btnCalculatePrimeNumberTo.addEventListener("click", function (e) {
-                var txtToNumber = document.getElementById("txtToNumber");
-                var pnlResult = document.getElementById("result-calculatePrimeNumberTo");
-                var workerData = new WorkerData(txtToNumber.value);
-                var result = appCache.get("calculatePrimeNumberTo", workerData);
-                if (result === undefined) {
-                    calculate.calculatePrimeNumberTo(txtToNumber.value).then(function (response) {
-                        workerData.result = response;
-                        appCache.pushSafe("calculatePrimeNumberTo", workerData);
-                        pnlResult.innerHTML = response;
-                    }, function(error) {
-                        pnlResult.innerHTML = error;
-                    });
+                WinJS.Application.onsettings = function (e) {
+                    e.detail.applicationcommands = { "preferences": { title: "Preferences", href: "/pages/settings/preferences.html" } };
+                    WinJS.UI.SettingsFlyout.populateSettings(e);
+                };
+                
+                if (nav.location) {
+                    nav.history.current.initialPlaceholder = true;
+                    return nav.navigate(nav.location, nav.state);
                 } else {
-                    pnlResult.innerHTML = result.result;
+                    return nav.navigate(Application.navigator.home);
                 }
-            });
-            
-            var btnCalculateFirstNumbers = document.getElementById("btnCalculateFirstNumbers");
-            btnCalculateFirstNumbers.addEventListener("click", function (e) {
-                var txtToNumber = document.getElementById("txtNumber");
-                var txtStopNumber = document.getElementById("txtStopNumber");
-                var pnlResult = document.getElementById("result-calculateFirstNumbers");
-                var workerData = new WorkerData(txtToNumber.value, txtStopNumber.value);
-                var result = appCache.get("calculateFirstNumbers", workerData);
-                if (result === undefined) {
-                    calculate.calculateFirstNumbers(txtToNumber.value, txtStopNumber.value).then(function (response) {
-                        workerData.result = response;
-                        appCache.pushSafe("calculateFirstNumbers", workerData);
-                        pnlResult.innerHTML = response;
-                    }, function (error) {
-                        pnlResult.innerHTML = error;
-                    });
-                } else {
-                    pnlResult.innerHTML = result.result;
-                }
-            });
-            
-            var btnCalculateFromRange = document.getElementById("btnCalculateFromRange");
-            btnCalculateFromRange.addEventListener("click", function (e) {
-                var txtStartNumber = document.getElementById("txtStartNumber");
-                var txtEndNumber = document.getElementById("txtEndNumber");
-                var pnlResult = document.getElementById("result-calculateFromRange");
-                var workerData = new WorkerData(txtStartNumber.value, txtEndNumber.value);
-                var result = appCache.get("calculateFromRange", workerData);
-                if (result === undefined) {
-                    calculate.calculateFromRange(txtStartNumber.value, txtEndNumber.value).then(function(response) {
-                        workerData.result = response;
-                        appCache.pushSafe("calculateFromRange", workerData);
-                        pnlResult.innerHTML = response;
-                    }, function(error) {
-                        pnlResult.innerHTML = error;
-                    });
-                } else {
-                    pnlResult.innerHTML = result.result;
-                }
-            });
+            }));
         }
-    };
+    });
 
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
-        // that needs to persist across suspensions here. You might use the
-        // WinJS.Application.sessionState object, which is automatically
-        // saved and restored across suspension. If you need to complete an
-        // asynchronous operation before your application is suspended, call
-        // args.setPromise().
+        // that needs to persist across suspensions here. If you need to 
+        // complete an asynchronous operation before your application is 
+        // suspended, call args.setPromise().
+        app.sessionState.history = nav.history;
     };
 
     app.start();
--- DesktopDevelopment/Win8JS/AppStorage/AppStorage/js/primeNumbersCalculator.js
+++ DesktopDevelopment/Win8JS/AppStorage/AppStorage/js/primeNumbersCalculator.js
@@ -1,15 +1,25 @@
 ﻿/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
 
-var PrimeNumbersCalculator = WinJS.Class.define(function() {
+var PrimeNumbersCalculator = WinJS.Class.define(function () {
+    this._maxWorkingWorkers = 3;
     this._workerCount = 0;
     this._workerCalculatePrimeNumberTo = Worker("/js/workers/calculatePrimeNumberTo.js");
     this._workerCalculateFirstNumbers = Worker("/js/workers/calculateFirstNumbers.js");
     this._workerCalculateFromRange = Worker("/js/workers/calculateFromRange.js");
 }, {
+    maxWorkingWorkers: {
+        get: function() {
+            this._maxWorkingWorkers = Windows.Storage.ApplicationData.current.localSettings.values["workerNumber"];
+            if (this._maxWorkingWorkers === undefined) {
+                this._maxWorkingWorkers = 3;
+            }
+            return this._maxWorkingWorkers;
+        }
+    },
     calculatePrimeNumberTo: function(number) {
         var self = this;
         return new WinJS.Promise(function(complete, error) {
-            if (self._workerCount < 3) {
+            if (self._workerCount < self.maxWorkingWorkers) {
                 self._workerCount++;
                 self._workerCalculatePrimeNumberTo.onmessage = function (event) {
                     self._workerCount--;
@@ -27,7 +37,7 @@ var PrimeNumbersCalculator = WinJS.Class.define(function() {
     calculateFirstNumbers: function(toNumber, stopNumber) {
         var self = this;
         return new WinJS.Promise(function(complete, error) {
-            if (self._workerCount < 3) {
+            if (self._workerCount < self.maxWorkingWorkers) {
                 self._workerCount++;
                 self._workerCalculateFirstNumbers.onmessage = function (event) {
                     self._workerCount--;
@@ -46,7 +56,7 @@ var PrimeNumbersCalculator = WinJS.Class.define(function() {
     calculateFromRange: function(startNumber, endNumber) {
         var self = this;
         return new WinJS.Promise(function(complete, error) {
-            if (self._workerCount < 3) {
+            if (self._workerCount < self.maxWorkingWorkers) {
                 self._workerCount++;
                 self._workerCalculateFromRange.onmessage = function (event) {
                     self._workerCount--;
