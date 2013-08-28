dd35c730493c0a1983da2a982fbeba79345fcb14
Dimitar Todorov <saykor@gmail.com>
2013-08-28 20:08:46 +0300

    add CollectionControls.jsproj add DataBinding.jsproj

---
+++ DesktopDevelopment/Win8JS/CollectionControls/CollectionControls/js/observableComputer.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    var computer = {
+        name: "",
+        imageUrl: "",
+        price: 0,
+        processor: {
+            modelName: "",
+            frequencyGHz: 0
+        },
+        manufacturer: ""
+    };
+
+    var ObservableComputer = WinJS.Binding.define(computer);
+
+    WinJS.Namespace.define("Data", {
+        getComputer: function (name, price, imageUrl, processorName, processorGHz, manufacturer) {
+            return new ObservableComputer({
+                name: name,
+                imageUrl: imageUrl,
+                price: price,
+                processor: {
+                    modelName: processorName,
+                    frequencyGHz: processorGHz
+                },
+                manufacturer: manufacturer
+            });
+        }
+    });
+})();
---
+++ DesktopDevelopment/Win8JS/Helpers/AppSettingToggle.js
@@ -0,0 +1,34 @@
+﻿// From http://brandonlive.com/2013/06/06/winjs-helper-appsettingtoggle-bind-a-toggleswitch-to-an-appdata-settings-value
+(function () {
+    "use strict";
+    var appView = Windows.UI.ViewManagement.ApplicationView;
+    var appViewState = Windows.UI.ViewManagement.ApplicationViewState;
+    WinJS.Namespace.define("BrandonUtils.Controls", {
+        // Options:
+        // settingName - Required. The name of the setting to toggle.
+        // getSettingContainer - Function to return setting container to use.
+        // enableRoaming - Use roaming setting container.
+        // If neither of the above are provided, the default local container is used.
+        AppSettingToggle: WinJS.Class.derive(WinJS.UI.ToggleSwitch, function (element, options) {
+            WinJS.UI.ToggleSwitch.apply(this, [ element, options ]);
+            WinJS.Utilities.addClass(element, "AppSettingToggle");
+            if (options.getSettingContainer) {
+                this.settingContainer = options.getSettingContainer();
+            }
+            else if (options.enableRoaming) {
+                this.settingContainer = Windows.Storage.ApplicationData.current.roamingSettings.values;
+            }
+            else {
+                this.settingContainer = Windows.Storage.ApplicationData.current.localSettings.values;
+            }
+
+            this.checked = this.settingContainer.hasKey(options.settingName) && this.settingContainer[options.settingName];    
+            this.onchange = this._onChange.bind(this);
+        },
+        {
+            _onChange: function () {
+                this.settingContainer.insert(this.settingName, this.checked);
+            },
+        }),
+    });
+})();
